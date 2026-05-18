1f9f4e5bc8e80d7f3e1c3d19616a1986a6faec31
Diya Shah <142000862+diya2405@users.noreply.github.com>
2026-05-18 15:49:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,10 +85,9 @@ def get_all_urls():
         'short_url': f'{request.host_url}{u[0]}'
     } for u in urls])
 
+init_db()
 
 if __name__ == '__main__':
     import os
-    os.makedirs('instance', exist_ok=True)
-    init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
